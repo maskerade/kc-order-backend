a85fdc94360ac15d44fc3db4031dc98832b4235c
maskerade <stefdefig@hotmail.co.uk>
2022-05-08 17:56:45 +0100

    feat(order): skeleton setup for order backend

--- lib/order-backend-stack.ts
+++ lib/order-backend-stack.ts
@@ -1,9 +1,41 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as apigw from '@aws-cdk/aws-apigatewayv2';
+import * as integrations from '@aws-cdk/aws-apigatewayv2-integrations';
+import fs = require('fs');
 
 export class OrderBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //DynamoDB Table
+    const table = new dynamodb.Table(this, 'contacts', {
+      partitionKey: { name: 'contactId', type: dynamodb.AttributeType.STRING }
+    });
+
+    // defines an AWS Lambda resource
+    const dynamoLambda = new lambda.Function(this, 'DynamoLambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
+      code: lambda.Code.fromInline(fs.readFileSync('src/lambda-api-add-contact.js', { encoding: 'utf-8' })),  // code loaded from the "lambda" directory
+      handler: 'index.handler',                // file is "lambda", function is "handler"
+      environment: {
+        CONTACTS_TABLE_NAME: table.tableName
+      }
+    });
+
+    // grant the lambda role read/write permissions to our table
+    table.grantReadWriteData(dynamoLambda);
+
+    // defines an API Gateway Http API resource backed by our "dynamoLambda" function.
+    let api = new apigw.HttpApi(this, 'Endpoint', {
+      defaultIntegration: new integrations.LambdaProxyIntegration({
+        handler: dynamoLambda
+      })
+    });
+
+    new cdk.CfnOutput(this, 'HTTP API Url', {
+      value: api.url ?? 'Something went wrong with the deploy'
+    });
   }
 }
